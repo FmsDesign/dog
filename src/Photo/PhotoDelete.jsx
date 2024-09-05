91f17bb2d186d97dd0761bcacdccbabeebcb3b82
fmsDesign <brandon300@ymail.com>
2024-09-05 13:37:02 -0300

    tela de loading, scroll infinito, efeito de carregamento de img

--- src/Photo/PhotoDelete.jsx
+++ src/Photo/PhotoDelete.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import style from "./PhotoDelete.module.css";
+import useFetch from "../Hooks/useFetch";
+import { PHOTO_DELETE } from "../Api";
 
-const PhotoDelete = () => {
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  async function handleClick() {
+    const confirm = window.confirm("Tem certeza que deseja deletar?");
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { response } = await request(url, options);
+
+      if (response.ok) window.location.reload();
+    }
+  }
+
   return (
-    <div>
-      <button className={style.delete}>Deletar</button>
-    </div>
+    <>
+      {loading ? (
+        <button disabled className={style.delete}>
+          Deletando...
+        </button>
+      ) : (
+        <button className={style.delete} onClick={handleClick}>
+          Deletar
+        </button>
+      )}
+    </>
   );
 };
 
