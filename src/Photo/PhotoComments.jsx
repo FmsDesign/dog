49a2384e831b2043a620edceabe63d15eda2e65e
fmsDesign <brandon300@ymail.com>
2024-09-05 06:16:06 -0300

    ajustes na página de modal e comentários

--- src/Photo/PhotoComments.jsx
+++ src/Photo/PhotoComments.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import PhotoCommentsForm from "./PhotoCommentsForm";
+import { UserContext } from "../UserContext";
+import styles from "./PhotoComments.module.css";
 
-const PhotoComments = () => {
-  return <div>oi</div>;
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const commentsSection = React.useRef(null);
+  const { login } = React.useContext(UserContext);
+
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+    console.log(commentsSection);
+  }, [comments]);
+
+  return (
+    <>
+      <ul ref={commentsSection} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      <div>
+        {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+      </div>
+      ;
+    </>
+  );
 };
 
 export default PhotoComments;
---
+++ src/Photo/PhotoDelete.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./PhotoDelete.module.css";
+
+const PhotoDelete = () => {
+  return (
+    <div>
+      <button className={style.delete}>Deletar</button>
+    </div>
+  );
+};
+
+export default PhotoDelete;
